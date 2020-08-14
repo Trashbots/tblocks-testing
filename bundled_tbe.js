bae380ae9de09a9f720cadddb1ec485dd1f058e6
axk5209 <arhamkothari@utexas.edu>
2020-08-14 15:30:28 -0500

    More printing time

--- bundled_tbe.js
+++ bundled_tbe.js
@@ -10666,7 +10666,7 @@ module.exports = function () {
 
 					if (block.name === 'print') {
 						var x = conductor.getPrintVal(block.controllerSettings.data); //value
-						duration = x.toString().length * 1000; //digits * 1000
+						duration = (x.toString().length + 1) * 1000; //digits * 1000
 					}
 					if (block.count === null || block.count === undefined) {
 						block.count = block.controllerSettings.data.duration;
@@ -10887,7 +10887,7 @@ module.exports = function () {
 			}
 		}
 		console.log('conductor print', d.print, d.variable, d.sensor, val);
-		return Math.trunc(val);
+		return Math.trunc(val); //truncated to an integer
 	};
 
 	conductor.playSingleChain = function () {
